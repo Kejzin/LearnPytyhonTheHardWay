5c0409b10a2548accdab3a55dcf68c0e369fe787
Konrad Janowski <kejzin@gmail.com>
2020-11-07 13:28:20 +0100

    [SD2] Ex5

--- ex5.py
+++ ex5.py
@@ -5,10 +5,13 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+height_cm = height * 2.53
+weight_kg = weight * 0.43
 
 print(f"Let's talk about {name}.")
 print(f"He's {height} inchess tall.")
 print(f"He's {weight} pounds heavy.")
+print(f"He would be {weight_kg} kg heavy and {height_cm} cm tall if he will be an European")
 print("Actually that's not to heavy.")
 print(f"He's hot {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffe.")
