240a3a7f9573dc13be6470fe9ed8ea695004f78d
Konrad Janowski <kejzin@gmail.com>
2020-12-02 11:32:22 +0100

    [SD2] Ex14

--- ex14.py
+++ ex14.py
@@ -1,12 +1,15 @@
 from sys import argv
 
-script, user_name = argv
-something_else = '> '
+script, user_name, thoughts = argv
+something_else = 'AA'
 
 print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(something_else)
+
+print(f"What do you think?")
+lives = input(thoughts)
 
 print(f"Where do you live {user_name}?")
 lives = input(something_else)
